93334e9f52981fdc87e167deb0832886c9744c18
r1oga <38692952+r1oga@users.noreply.github.com>
2022-12-07 22:08:28 +0100

    refactor

---
+++ src/Provider.ts
@@ -0,0 +1,63 @@
+import { readFileSync, writeFileSync } from 'fs'
+
+import { SAMPLE_SIZE } from './constants'
+
+export abstract class Provider<U> {
+  ids: string[]
+  users: Array<U & { id: string }>
+
+  constructor(public file: string) {
+    const _users = this.getLocalUsers()
+    this.users = _users
+    this.ids = _users.map((user) => user.id)
+  }
+
+  getLocalUsers(): typeof this.users {
+    try {
+      return JSON.parse(readFileSync(this.file, 'utf8'))
+    } catch (e) {
+      return []
+    }
+  }
+
+  abstract randomIds(): string[]
+
+  abstract fetchUsers(ids: string[]): Promise<any>
+
+  async loadUsers(size = SAMPLE_SIZE) {
+    while (this.users.length <= size) {
+      let data
+
+      try {
+        data = (await this.fetchUsers(this.randomIds())).data
+      } catch (e) {
+        continue
+      }
+
+      if (data !== undefined) {
+        const [users, ids] = data.reduce(
+          // @ts-expect-error
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          ([users, ids], { id, public_metrics, username, verified }) => {
+            // no users for this id
+            if (id === undefined) return [users, ids]
+
+            users.push({ id, ...public_metrics, username, verified })
+            ids.push(id)
+
+            console.log(users)
+            return [users, ids]
+          },
+          [[], []],
+        )
+
+        this.users.push(...users)
+        this.ids.push(...ids)
+      }
+    }
+  }
+
+  writeUsers() {
+    writeFileSync(this.file, JSON.stringify(this.users))
+  }
+}
--- src/Twitter.ts
+++ src/Twitter.ts
@@ -1,15 +1,28 @@
+import { join } from 'path'
+
 import { Client } from 'twitter-api-sdk'
 
 import { config } from './config'
+import { Provider } from './Provider'
+import { randomTwitterIds } from './utils'
 
-class Twitter {
+export class Twitter extends Provider<{
+  public_metrics: any
+  username: string
+  verified: boolean
+}> {
   client: Client
 
   constructor() {
+    super(join(__dirname, '..', 'twitter.json'))
     this.client = new Client(config.twitter.bearerToken)
   }
 
-  async getMetrics(ids: string[]) {
+  randomIds() {
+    return randomTwitterIds({ usedIds: this.ids })
+  }
+
+  async fetchUsers(ids: string[]) {
     return this.client.users.findUsersById({
       ids,
       'user.fields': ['public_metrics', 'verified'],
