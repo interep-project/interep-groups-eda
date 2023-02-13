ff02ad9887fa031b994ac1b89cad671ef4caa7cb
r1oga <38692952+r1oga@users.noreply.github.com>
2023-02-13 19:50:24 +0100

    Simulate different reddit reputation algos
    Signed-off-by: r1oga <38692952+r1oga@users.noreply.github.com>

---
+++ notebooks/tiers.py
@@ -0,0 +1,9 @@
+# each tier has a number key and dict value with keys name and color that are both strings
+# keys go from 0 to 4
+# values (color, name): (white, commoner), (blue, up and coming), (purple, established), (gold, star), (red, icon)
+
+TIERS = {0: {'color': 'white', 'name': 'commoner'},
+         1: {'color': 'blue', 'name': 'up and coming'},
+         2: {'color': 'purple', 'name': 'established'},
+         3: {'color': 'gold', 'name': 'star'},
+         4: {'color': 'red', 'name': 'icon'}}
